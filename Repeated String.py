56ee0bcca3ad9a8cd9bdf0518e8b958d8b87125f
ahmedsy96 <33118284+ahmedsy96@users.noreply.github.com>
2020-03-22 23:56:59 +0300

    Add files via upload

---
+++ Repeated String.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 22 23:54:19 2020
+
+@author: jadee
+"""
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the repeatedString function below.
+def repeatedString(s, n):
+
+    d=list(s)
+    sayac =0
+    for i in range(len(d)):
+        if d[i] == 'a':
+            sayac+=1
+    k = int((n / len(d))) * sayac 
+    if n % len(d) == 0 : 
+        return(k)
+    else : 
+        l = 0
+        for i in range (n%len(d)): 
+            if d[i]=='a': 
+                l+=1
+        return(k+l)
